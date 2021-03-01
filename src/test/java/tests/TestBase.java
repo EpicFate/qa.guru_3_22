f841fd522a908c11ffdd5002983757021916f270
Admin <bogtinys20@gmail.com>
2021-03-01 22:04:20 +0300

    boom!

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
@@ -20,13 +19,12 @@ public class TestBase {
     }
 
     @AfterEach
+    @Step("Attachments")
     public void afterEach(){
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
         attachVideo();
-
         closeWebDriver();
     }
-
 }
