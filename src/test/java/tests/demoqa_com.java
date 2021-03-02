0f083b9df2481222f2e8d3ad8b59566ade892101
Admin <bogtinys20@gmail.com>
2021-03-02 14:58:47 +0300

    add radio_button test

--- src/test/java/tests/demoqa_com.java
+++ src/test/java/tests/demoqa_com.java
@@ -118,8 +118,9 @@ public class demoqa_com extends TestBase {
     }
 
     @Test
+    @Feature("radio_buttons")
     @Owner("EpicFate")
-    @DisplayName("radio_button")
+    @DisplayName("radio_button_Yes-or-Impressive")
     @Link(url = "https://demoqa.com/radio-button", name = "https://demoqa.com/radio-button")
 
     void radio_button() {
